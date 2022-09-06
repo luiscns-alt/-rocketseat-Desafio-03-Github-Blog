f7614688662a1b1c750002b088401f668d785722
Luis Carlos <luiscarlosnors@gmail.com>
2022-09-06 13:02:19 -0300

    chore: add lint, prettier ant router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
 export function App() {
   return (
-    <div>
-      <div>Apppp</div>
-    </div>
-  );
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  )
 }
